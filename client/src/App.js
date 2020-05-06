9b316debdfa09b0314eff65fc4cdf9d49bff433f
임진택 <kococo9504@gmail.com>
2020-05-06 14:50:13 +0900

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Test from './components/input';
+function App() {
+  return (
+    <div className="App">
+      <Test/>
+    </div>
+  );
+}
+
+export default App;
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Tensorflow Train Tools
+
+React + Flask + Tensorflow Tool
